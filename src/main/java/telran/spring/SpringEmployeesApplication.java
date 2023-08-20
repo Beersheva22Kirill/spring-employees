9ec88ea67539ca73d87b67f7c167d548fccdca57
kirill2884 <kirill2884@gmail.com>
2023-08-20 11:34:45 +0300

    pom file and annotation @ComponentScan

--- src/main/java/telran/spring/SpringEmployeesApplication.java
+++ src/main/java/telran/spring/SpringEmployeesApplication.java
@@ -2,9 +2,12 @@ package telran.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"telran"})
 public class SpringEmployeesApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmployeesApplication.class, args);
